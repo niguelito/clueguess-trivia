917f9b4033eca8f43e2a14eabf240f7376137349
Jack <JacksonEverettPorter@gmail.com>
2026-03-30 05:11:47 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3714571635-5200b",
+  "appId": "1:923918535116:web:3cbc5686d4ac5bd16a7569",
+  "apiKey": "AIzaSyCupvGIHNIU9M4QYQoqph1VAC7Od8WG4CQ",
+  "authDomain": "studio-3714571635-5200b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "923918535116"
+};
